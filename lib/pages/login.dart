df23874f0eef7f7075e4699b4f331ef6d7721a80
geraldin <geraldinpinzonb@gmail.com>
2021-03-02 00:12:57 -0500

    Titulo iniciar sesion, input email

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,8 +4,49 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text(""),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 1,
+            child: Text(
+              'Iniciar sesion',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: MediaQuery.of(context).textScaleFactor * 24,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: TextFormField(
+              onChanged: (value) => print('Hola'),
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                errorText: 'Ingreselo de nuevo',
+                labelText: 'Iniciar sesion',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
