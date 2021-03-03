2b76647a0da09b65dbc3639a708c821e207bb219
geraldin <geraldinpinzonb@gmail.com>
2021-03-03 13:47:13 -0500

    Se agrega card en home, foto e informacion de usuario

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:GestureApp/pages/card.dart' as car;
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.08,
-        child: RaisedButton(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
           elevation: 0,
-          disabledColor: Colors.amber,
-          child: Text("Iniciar Sesion"),
-          splashColor: Colors.amber,
-          color: Colors.blueAccent,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
-              side: BorderSide(color: Colors.red)),
-          onPressed: () {
-            Navigator.pop(context);
-          },
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            car.Card("assets/images/laura.jpg", "Mary Lopez",
+                "5 review 8 photos", "There is an amazing app")
+          ],
+        ));
   }
 }
