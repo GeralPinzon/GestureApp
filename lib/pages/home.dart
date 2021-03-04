ea64235500d65d445010126f4b599bda298af7fe
Yesid Giovanny Sanabria <yesid@doublevpartners.com-at-074803668141>
2021-03-03 22:20:07 -0500

    Cambio diseño home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:GestureApp/pages/card.dart' as car;
 import 'package:GestureApp/widgets/appBarTransparent.dart';
+import 'package:flutter/cupertino.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xFFE3F2FD),
       body: Column(
         children: [
-          car.Card("assets/images/laura.jpg", "Mary Lopez", "5 review 8 photos",
-              "There is an amazing app"),
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF4268D3), Color(0xFF584DC1)],
+                    begin: FractionalOffset(0.2, 0.9),
+                    end: FractionalOffset(1.0, 0.6),
+                    stops: [0.0, 0.9],
+                    tileMode: TileMode.clamp,
+                  ),
+                ),
+              ),
+              Center(
+                heightFactor: 1.3,
+                child: Container(
+                  margin: EdgeInsets.all(20),
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/laura.jpg'),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    shape: BoxShape.rectangle,
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 7.0))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
       bottomNavigationBar: Theme(
