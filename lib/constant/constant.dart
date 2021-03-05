f5cae7fb26d7cfb8fa2dbb2aa899ee27ce6ae2dc
Yesid Giovanny Sanabria <yesid@doublevpartners.com-at-074803668141>
2021-03-04 20:37:36 -0500

    constantes
    Se crea archivo de constantes para hacer mas facil la mantenibilidad del app

---
+++ lib/constant/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color titleColor = Color(0xFF324788);
+const Color backgroundButton = Color(0xFF332275);
+const Color colorTextButton = Colors.white;
+const FontWeight fontweightTitle = FontWeight.w800;
+const double elevationButton = 6;
+const Color backgroundApp = Color(0xFFE3F2FD);
