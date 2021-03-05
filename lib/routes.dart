27f433ed2a4e0302a4302d1701ab2e1d54d766fd
Yesid Giovanny Sanabria <yesid@doublevpartners.com-at-074803668141>
2021-03-04 20:38:42 -0500

    agreagar ruta de pantalla principal

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:GestureApp/pages/login.dart';
 import 'package:GestureApp/pages/home.dart';
 import 'package:GestureApp/pages/register.dart';
+import 'package:GestureApp/pages/principal.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  'loginScreen': (BuildContext context) => Login(),
+  'login': (BuildContext context) => Login(),
   'home': (BuildContext context) => Home(),
   'register': (BuildContext context) => Register(),
+  'principal': (BuildContext context) => Principal(),
   '/': (BuildContext context) => Login(),
 };
